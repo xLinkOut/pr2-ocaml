6c754b83085eea0776fa69a44ae97949deb578af
Luca <luca.cirillo5@gmail.com>
2020-02-03 14:54:39 +0100

    add: added expected tests value; added type checking for some primitive functions

--- src/typechecker-statico.ml
+++ src/typechecker-statico.ml
@@ -33,9 +33,15 @@ type exp =
 
 (* Ambiente *)
 type 't env = (string * 't) list;;
+
 let rec lookup x y = match x with
     | (i1,e1) :: x1 -> if y = i1 then e1 else lookup x1 y
     | [] -> failwith("wrong env")
+;;
+
+let bind (env : 't env) (i : ide) (v : 't) = function 
+    x -> if x = i  then v
+         else lookup env x
 ;;
 
 (* Valori esprimibili *)
@@ -55,7 +61,64 @@ let rec checker (e : exp) (env : evT env) : evT =
     match e with
     | Eint i -> Int
     | Ebool b -> Bool
-    | Den s -> String
+    | Sum (e1, e2) -> 
+        let t1 = checker e1 env in
+            let t2 = checker e2 env in
+                (match t1, t2 with
+                    (Int, Int) -> Int
+                    | _ -> failwith("Errore di tipo"))
+    | Diff(e1, e2) -> 
+        let t1 = checker e1 env in
+            let t2 = checker e2 env in
+                (match t1, t2 with
+                    | (Int, Int) -> Int
+                    | _ -> failwith("Errore di tipo"))
+    | Prod(e1, e2) ->
+        let t1 = checker e1 env in
+            let t2 = checker e2 env in
+                (match t1, t2 with
+                    | (Int, Int) -> Int
+                    | _ -> failwith("Errore di tipo"))
+    | Minus(e1) ->
+        let t1 = checker e1 env in
+            (match t1 with
+                | Int -> Int
+                | _ -> failwith("Errore di tipo"))
+    | Eq(e1, e2) -> 
+        let t1 = checker e1 env in
+            let t2 = checker e2 env in
+                (match t1, t2 with
+                    | (Int, Int) -> Bool
+                    | _ -> failwith("Errore di tipo"))
+    | IsZero(e1) ->
+        let t1 = checker e1 env in
+            (match t1 with
+                | Int -> Bool
+                | _ -> failwith("Errore di tipo"))
+    | Or(e1, e2) -> 
+        let t1 = checker e1 env in
+            let t2 = checker e2 env in
+                (match t1, t2 with
+                    (Bool, Bool) -> Bool
+                    | _ -> failwith("Errore di tipo"))
+    | And(e1, e2) ->
+        let t1 = checker e1 env in
+            let t2 = checker e2 env in
+                (match t1, t2 with
+                    (Bool, Bool) -> Bool
+                    | _ -> failwith("Errore di tipo"))
+    | Not(e1) -> 
+        let t1 = checker e1 env in
+            (match t1 with
+                | Bool -> Bool
+                | _ -> failwith("Errore di tipo"))
+    | Ifthenelse(e1, e2, e3) -> (match checker e1 env with
+        | Bool -> 
+            let t2 = checker e2 env in
+                let t3 = checker e3 env in
+                    if t2 = t3 then t2
+                    else failwith("branch <if> e branch <else> hanno tipi differenti")
+        | _ -> failwith("<guardia> non booleana"))
     | Dict l -> (match l with
         | [] -> DictValue
         | _ -> 
@@ -95,12 +158,29 @@ let rec checker (e : exp) (env : evT env) : evT =
 
 let typeCheck e = checker e [];;
 
-typeCheck (Dict([]));;
+typeCheck (Dict([]));; 
+(* DictValue *)
+
 typeCheck (Dict([("key",Eint(1))]));;
+(* DictValue *)
+
 typeCheck (Dict([("key",Eint(1));("key2",Eint(2))]));;
+(* DictValue *)
+
 typeCheck (Insert("key",Eint(5),Dict([])));;
+(* DictValue *)
+
 typeCheck (Delete("key",Dict([])));;
+(* DictValue *)
+
 typeCheck (HasKey("key",Dict([])));;
+(* Bool *)
+
 typeCheck (Iterate(Fun("y",Sum(Den "y",Eint(1))),Dict([])));;
+(* DictValue *)
+
 typeCheck (Fold(Fun("y",Sum(Den "y",Eint(0))),Dict([])));;
+(* Int *)
+
 typeCheck (Filter(["key"],Dict([("key",Eint(1));("key2",Eint(2))])));;
+(* DictValue *)
